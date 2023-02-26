60840ffe09f0ea5e8b4bed2cfb818a4a29ab163f
Harshith Rao <105939674+harshithrao07@users.noreply.github.com>
2023-02-26 11:54:04 +0530

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import HighlightOutlinedIcon from '@mui/icons-material/HighlightOutlined';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <HighlightOutlinedIcon />
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
